ed06ae5cc525cc983b039ab6965b9ff5754efe43
Johann Folta <jfolta@kyruus.com>
2020-04-18 11:24:06 -0400

    setup scoreboard; add Navbar, Player

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,30 @@
 <script>
-	export let name;
+  import Navbar from './Navbar.svelte'
+  import Player from './Player.svelte'
+
+  let players = [
+    {
+      name: 'Ash Ketchum',
+      points: 100
+    },
+    {
+      name: 'Misty Yawa',
+      points: 100
+    },
+    {
+      name: 'Brock',
+      points: 100
+    }
+  ];
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Navbar />
+<div class="container">
+  {#if players.length === 0}
+    <p>No Players</p>
+  {:else}
+    {#each players as player}
+      <Player name={player.name} points={player.points} />
+    {/each}
+  {/if}
+</div>
